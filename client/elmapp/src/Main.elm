66307a96ac01e1cc11565034b24d8094bc6fc184
chyno <jchynowe1@yahoo.com>
2019-06-11 14:16:36 -0400

    elm make works

---
+++ client/elmapp/src/Main.elm
@@ -0,0 +1,12 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html)
+
+update msg _ = msg
+
+view model =
+  Html.text model
+
+main =
+    Browser.sandbox { init = "Hello, World from Elm!", update = update, view = view }
